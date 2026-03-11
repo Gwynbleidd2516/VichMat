52d80774ff4f7c2b2c6bf7c974e8deddfc4f5f11
NikitaVyaznikov <nvyiznik@gmail.com>
2026-03-11 14:39:28 +0300

    IScene is added

---
+++ src/Scene/IScene.h
@@ -0,0 +1,12 @@
+#ifndef ISCENE
+#define ISCENE
+
+class IScene
+{
+public:
+    virtual void reshape(int w, int h) = 0;
+    virtual void keyboard(unsigned char key, int x, int y) = 0;
+    virtual void display() = 0;
+};
+
+#endif
