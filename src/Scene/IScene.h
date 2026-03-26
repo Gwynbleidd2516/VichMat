33653459492b55bc3a3627f745209415fa19c88a
NikitaVyaznikov <nvyiznik@gmail.com>
2026-03-26 10:08:44 +0300

    IScene is changed for imgui

--- src/Scene/IScene.h
+++ src/Scene/IScene.h
@@ -1,24 +1,13 @@
 #ifndef ISCENE
 #define ISCENE
 
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
 class IScene
 {
 public:
-    virtual void reshape(int w, int h) = 0;
-    virtual void keyboard(unsigned char key, int x, int y) = 0;
     virtual void display() = 0;
-    virtual void timeFunc(int value) = 0;
-
-    void timer(int value)
-    {
-        timeFunc(value);
-        glutPostRedisplay();
-        glutTimerFunc(16, [](int val)
-                      {
-        IScene* scenePtr = static_cast<IScene*>(glutGetWindowData());
-        if (scenePtr) scenePtr->timer(val); }, 0);
-    }
-
 };
 
 #endif
